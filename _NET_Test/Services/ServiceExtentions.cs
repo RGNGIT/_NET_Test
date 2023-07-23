d86297f3b8892f08db34bca1be62ec4bbdd768f4
RGNGIT <vnifedow@bk.ru>
2023-07-23 14:46:15 +0500

    Some patch

--- _NET_Test/Services/ServiceExtentions.cs
+++ _NET_Test/Services/ServiceExtentions.cs
@@ -1,35 +1,36 @@
-﻿using _NET_Test.Repositories;
+﻿using _NET_Test.DatabaseModels;
+using _NET_Test.Repositories;
 
 namespace _NET_Test.Services
 {
 
 	public interface IUsersService
 	{
-		public async Task<User> AddNew(UsersRepository repo, string username, string password);
-		public async Task<User?> FetchByUsername(UsersRepository repo, string username);
+		public Task<User> AddNew(UsersRepository repo, string username, string password);
+		public Task<User?> FetchByUsername(UsersRepository repo, string username);
 		public AuthUser JwtToUser(string jwtHeader);
 	}
 
 	public interface IActorsService
 	{
-		public async Task<Actor?> Fetch(ActorsRepository repository, int id);
-		public async Task<List<Movie>> FetchMovies(ActorsRepository repository, int id);
-		public async Task<List<Rating>> FetchRatings(ActorsRepository repository, int id);
-		public async Task<Actor> AddNew(ActorsRepository repository, string name, string surname);
-		public async Task<Actor> AddRating(ActorsRepository actorsRepository, UsersRepository usersRepository, Rating rating, int UserId);
-		public async Task<Actor> AddMovie(ActorsRepository actorsRepository, MoviesRepository moviesRepository, ActorsMoviesRepository actorsMoviesRepository, int ActorId, int MovieId);
-		public async Task<List<Actor>> FetchAll(ActorsRepository actorsRepo);
+		public Task<Actor?> Fetch(ActorsRepository repository, int id);
+		public Task<List<Movie>> FetchMovies(ActorsRepository repository, int id);
+		public Task<List<Rating>> FetchRatings(ActorsRepository repository, int id);
+		public Task<Actor> AddNew(ActorsRepository repository, string name, string surname);
+		public Task<Actor> AddRating(ActorsRepository actorsRepository, UsersRepository usersRepository, Rating rating, int UserId);
+		public Task<Actor> AddMovie(ActorsRepository actorsRepository, MoviesRepository moviesRepository, ActorsMoviesRepository actorsMoviesRepository, int ActorId, int MovieId);
+		public Task<List<Actor>> FetchAll(ActorsRepository actorsRepo);
 	}
 
 	public interface IMoviesService
 	{
-		public async Task<Movie?> Fetch(MoviesRepository repository, int id);
-		public async Task<List<Actor>> FetchActors(MoviesRepository repository, int id);
-		public async Task<List<Rating>> FetchRatings(MoviesRepository repository, int id);
-		public async Task<List<Movie>> FetchAll(MoviesRepository moviesRepo);
-		public async Task<Movie> AddNew(MoviesRepository moviesRepo, string name);
-		public async Task<Movie> AddRating(MoviesRepository moviesRepository, UsersRepository usersRepository, Rating rating, int UserId);
-		public async Task<Movie> AddActor(ActorsRepository actorsRepository, MoviesRepository moviesRepository, ActorsMoviesRepository actorsMoviesRepository, int MovieId, int ActorId);
+		public Task<Movie?> Fetch(MoviesRepository repository, int id);
+		public Task<List<Actor>> FetchActors(MoviesRepository repository, int id);
+		public Task<List<Rating>> FetchRatings(MoviesRepository repository, int id);
+		public Task<List<Movie>> FetchAll(MoviesRepository moviesRepo);
+		public Task<Movie> AddNew(MoviesRepository moviesRepo, string name);
+		public Task<Movie> AddRating(MoviesRepository moviesRepository, UsersRepository usersRepository, Rating rating, int UserId);
+		public Task<Movie> AddActor(ActorsRepository actorsRepository, MoviesRepository moviesRepository, ActorsMoviesRepository actorsMoviesRepository, int MovieId, int ActorId);
 	}
 
 	public interface IHashService 
